93941586c8e94c1748d6a40749d2336ef9e3877f
magistr <x2wing@gmail.com>
2017-02-05 17:23:39 +0500

    resolve organization error

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,8 +1,99 @@
-duplicity  --no-encryption --volsize=1024 --include=/home/magistr/zgate  --exclude=/** / file:///backup-duplicity
+# -*- coding: utf-8 -*- 
+import numpy 
 
 
-архивация полная:
-sudo duplicity full --no-encryption --no-compression /media/flash/ file:///media/DATA/backup/flash072016
+def checker(V):
+	for i in xrange(1,len(V)):
+		if V[i-1]>V[i]:
+			print("сортировка говно")
+			return
 
-разархивация:
-sudo duplicity restore --no-encryption --no-compression  file:///restore/ /home/magistr/backup2
+def BubleSort(V):
+	counter=0
+	for i in xrange(V.__len__()):
+		#print(len(V))
+		F=0
+		for j in xrange(1,V.__len__()-i):
+			counter+=1
+			if V[j-1]>V[j]:
+				V[j-1],V[j]=V[j],V[j-1] # обмен
+				F=1
+				
+		if F==0:
+			print("counter=",counter)
+			return
+	print("counter=",counter)
+
+def InsertionSort(V):
+	counter=0
+	for i in xrange(1,V.__len__()):
+		j=i
+		
+		while j>0 and V[j]<V[j-1]:
+			counter+=1
+			V[j-1],V[j]=V[j],V[j-1]
+			j-=1
+
+				
+	print("counter=",counter)
+
+
+
+
+		
+
+
+
+def SelectSort(arr):
+	counter=0
+	i = len(arr)
+	while i > 1:
+		max = 0
+		for j in xrange(i):
+			counter+=1
+			if arr[j] > arr[max]:
+				max = j
+		arr[i-1], arr[max] = arr[max], arr[i-1]
+		i -= 1
+	print("counter=",counter)
+
+
+def ShellSort(a):
+	counter=0
+	def new_increment(a): 
+		i = int(len(a) / 2)
+		yield i
+		while i != 1:
+			
+			if i == 2:
+				i = 1
+			else:
+				i = int(numpy.round(i/2.2))
+			yield i
+	for increment in new_increment(a):
+		for i in xrange(increment, len(a)):
+			for j in xrange(i, increment-1, -increment):
+				counter+=1
+				if a[j - increment] < a[j]:
+					break
+				a[j],a[j - increment] = a[j - increment],a[j]
+	print("counter=",counter)
+      
+
+
+
+
+
+
+
+Vect=numpy.random.randint(0, 255, 1000000)
+#Vect=[10,9,8,2,3,4,5,3,2,1]
+#VectCopy=Vect[:]
+print("not sorted",Vect)
+#checker(Vect)
+ShellSort(Vect)
+#BubleSort(VectCopy)
+checker(Vect)
+#checker(VectCopy)
+print("sorted", Vect)
+#print("sorted", VectCopy)
